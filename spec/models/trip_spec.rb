3b0273d322aad6ca62445f54349b409e2f1b271d
Hugh Morrison <hmorri32@gmail.com>
2017-10-11 14:14:14 -0600

    test all trip class methods

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -114,6 +114,146 @@ RSpec.context Trip do
         expect(Trip.shortest_ride).to eql(16)
       end
     end
+
+    describe '.station_most_starting_rides' do
+      it 'returns the station name with the most starting rides' do
+        expect(Trip.station_most_starting_rides).to eql("chillest")
+      end
+    end
+
+    describe '.station_most_ending_rides' do
+      it 'returns the station name with the most ending rides' do
+        expect(Trip.station_most_ending_rides).to eql("surf shack")
+      end
+    end
+
+    describe '.monthly_rides' do
+      it 'returns the a hash with month and ride count' do
+        expect(Trip.monthly_rides.class).to eql(Hash)
+        expect(Trip.monthly_rides).to have_value(3)
+      end
+    end
+
+    describe '.yearly_rides' do
+      it 'returns a hash with year and ride count' do
+        expect(Trip.yearly_rides.class).to eql(Hash)
+        expect(Trip.yearly_rides).to have_value(3)
+      end
+    end
+
+    describe '.ridden_bikes' do
+      it 'returns all bikes as a hash with bike id and ride count' do
+        expect(Trip.ridden_bikes.class).to eql(Hash)
+        expect(Trip.ridden_bikes).to eql({520=>2, 22=>1, 12=>1, 2=>1})
+      end
+    end
+
+    describe '.most_ridden_bike' do
+      it 'returns the most ridden bike with id and ride count' do
+        expect(Trip.most_ridden_bike.class).to eql(Array)
+        expect(Trip.most_ridden_bike).to eql([520, 2])
+      end
+    end
+
+    describe '.least_ridden_bike' do
+      it 'returns the least ridden bike with id and ride count' do
+        expect(Trip.least_ridden_bike.class).to eql(Array)
+        expect(Trip.least_ridden_bike).to eql([22, 1])
+      end
+    end
+
+    describe '.subscription_breakdown' do
+      it 'returns hash with breakdown of subscription type' do
+        expect(Trip.subscription_breakdown.class).to eql(Hash)
+        expect(Trip.subscription_breakdown).to eql({"Customer"=>2, "Subscriber"=>3})
+      end
+    end
+
+    describe '.subscriber_count' do
+      it 'returns total subscriber number' do
+        expect(Trip.subscriber_count.class).to eql(Integer)
+        expect(Trip.subscriber_count).to eql(3)
+      end
+    end
+
+    describe '.customer_count' do
+      it 'returns total customer number' do
+        expect(Trip.customer_count.class).to eql(Integer)
+        expect(Trip.customer_count).to eql(2)
+      end
+    end
+
+    describe '.subscriber_percentage' do
+      it 'returns percentage of total that are subscribers' do
+        expect(Trip.subscriber_percentage.class).to eql(Float)
+        expect(Trip.subscriber_percentage).to eql(60.0)
+      end
+    end
+
+    describe '.customer_percentage' do
+      it 'returns percentage of total that are customers' do
+        expect(Trip.customer_percentage.class).to eql(Float)
+        expect(Trip.customer_percentage).to eql(40.0)
+      end
+    end
+
+    describe '.date_with_highest_trips' do
+      it 'returns date and ride count on highest day' do
+        expect(Trip.date_with_highest_trips.class).to eql(Array)
+        expect(Trip.date_with_highest_trips.first.class).to eq(Date)
+        expect(Trip.date_with_highest_trips.last).to eq(3)
+      end
+    end
+
+    describe '.date_with_lowest_trips' do
+      it 'returns date and ride count on lowest day' do
+        expect(Trip.date_with_lowest_trips.class).to eql(Array)
+        expect(Trip.date_with_lowest_trips.first.class).to eq(Date)
+        expect(Trip.date_with_lowest_trips.last).to eq(2)
+      end
+    end
+
+    describe '.start_station' do
+      it 'returns start stations by trip id' do
+        expect(Trip.start_station(1).length).to eql(3)
+      end
+    end
+
+    describe '.rides_ended' do
+      it 'returns ride count by trip id' do
+        expect(Trip.rides_ended(1)).to eql(1)
+      end
+    end
+
+    describe '.frequent_destination' do
+      it 'returns frequent destination by id' do
+        expect(Trip.frequent_destination(1)).to eql("surf shack")
+      end
+    end
+
+    describe '.origin_destination' do
+      it 'returns origin destination by id' do
+        expect(Trip.origin_destination(1)).to eql("chillest")
+      end
+    end
+
+    describe '.busiest_date' do
+      it 'returns busiest date by trip ID' do
+        expect(Trip.busiest_date(1).class).to eql(Date)
+      end
+    end
+
+    describe '.frequent_zipcode' do
+      it 'returns busiest zipcode by trip ID' do
+        expect(Trip.frequent_zipcode(1)).to eql(94127)
+      end
+    end
+
+    describe '.frequent_bike' do
+      it 'returns busiest bike ID by trip ID' do
+        expect(Trip.frequent_bike(1)).to eql(520)
+      end
+    end
   end
 
   describe "Validates" do
