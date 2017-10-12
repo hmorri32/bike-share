215c030454186836b06182b29ac61a8474350ede
Tyjoo26 <Tyjoo21@gmail.com>
2017-10-12 06:26:05 -0600

    Build feature test for conditions/new_spec.rb and test passes

---
+++ spec/features/conditions/new_spec.rb
@@ -0,0 +1,9 @@
+describe 'conditions/new' do
+  before :each do
+    Condition.create(date:"2015-08-06", max_temperature: 43, mean_temperature: 35, min_temperature: 28, mean_humidity: 72, mean_visibility: 3, mean_wind_speed: 35, precipitation: 3)
+  end
+
+
+
+
+end
